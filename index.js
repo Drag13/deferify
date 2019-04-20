9e43794bd54c523e8978b477d7486a0fb776a064
drag13 <qwark@list.ru>
2019-04-20 18:41:57 +0300

    main file reworked to handle errors correctly

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const _fs = require('fs');
-const _path = require('path');
+#! /usr/bin/env node
 const _deferify = require('./deferify');
+const _file = require('./file');
 
-(function (fs, path, transform) {
+(function (file, transform) {
     const [rawPathToFile] = process.argv.slice(2);
-    const pathToFile = path.join(__dirname, rawPathToFile);
-    const file = fs.readFileSync(pathToFile, 'utf8');
 
-    const result = transform(file);
-    fs.writeFileSync(pathToFile, result, { encoding: 'utf8' });
-}(_fs, _path, _deferify));
+    if (rawPathToFile == undefined || rawPathToFile === '') {
+        console.error('ERROR: path to file is empty. Operation aborted.');
+        return -1
+    }
+
+    const pathToFile = file.normalizePath(rawPathToFile);
+
+    if (!file.exists(pathToFile)) {
+        console.error(`ERROR: file "${pathToFile}" not exists or not accessible. Operation aborted.`);
+        return -1;
+    }
+
+    let fileContent = '';
+    try {
+        fileContent = file.read(pathToFile);
+    } catch (e) {
+        console.error(`ERROR: unexpected error during reading file: ${e}. Operation aborted.`);
+        return -2;
+    }
+
+    const result = transform(fileContent);
+
+    try {
+        file.write(pathToFile, result);
+    } catch (e) {
+        console.error(`ERROR: unexpected error during writing file: ${e}. Operation aborted.`);
+        return -3;
+    }
+
+    console.log("SUCCESS. Everything is done. Wish you fastest app!");
+    return 0;
+}(_file, _deferify));
