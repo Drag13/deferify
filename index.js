edb1cbc3ab1137b904c42ca8bc2e7abb70ed2b4a
drag13 <qwark@list.ru>
2019-04-21 14:54:07 +0300

    draft for multiple foldefs

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 #! /usr/bin/env node
 const _deferify = require('./deferify');
 const _file = require('./file');
+const defaultPathToFile = '/dist';
 
 (function (file, transform) {
     console.log('STARTING: deferify starts');
-    const [rawPathToFile] = process.argv.slice(2);
-
-    if (rawPathToFile == undefined || rawPathToFile === '') {
-        console.error('ERROR: path to file is empty. Operation aborted.');
-        return -1
-    }
+    let [rawPathToFile] = process.argv.slice(2);
+    rawPathToFile = rawPathToFile || defaultPathToFile;
 
     const pathToFile = file.normalizePath(rawPathToFile);
 
-    if (!file.exists(pathToFile)) {
-        console.error(`ERROR: file "${pathToFile}" not exists or not accessible. Operation aborted.`);
-        return -1;
+    if (file.isFile(pathToFile)) {
+        return tryUpdateFile(pathToFile) ? 0 : -1;
+    } else {
+        const files = file.getAllIndexFiles(pathToFile);
+        console.log(files);
+        files.forEach(tryUpdateFile);
     }
 
-    let fileContent = '';
-    try {
-        fileContent = file.read(pathToFile);
-    } catch (e) {
-        console.error(`ERROR: unexpected error during reading file: ${e}. Operation aborted.`);
-        return -2;
+    function tryUpdateFile(filePath) {
+
+        if (!file.exists(filePath)) {
+            console.error(`ERROR: file "${filePath}" not exists or not accessible. Operation aborted.`);
+            return false;
+        }
+
+        let fileContent = '';
+        try {
+            fileContent = file.read(filePath);
+        } catch (e) {
+            console.error(`ERROR: unexpected error during reading file: ${e}. Operation aborted.`);
+            return false;
+        }
+
+        const result = transform(fileContent);
+
+        try {
+            file.write(filePath, result);
+        } catch (e) {
+            console.error(`ERROR: unexpected error during writing file: ${e}. Operation aborted.`);
+            return false;
+        }
+
+        console.log("SUCCESS. Everything is done. Wish you fastest app!");
+
+        return true;
     }
-
-    const result = transform(fileContent);
-
-    try {
-        file.write(pathToFile, result);
-    } catch (e) {
-        console.error(`ERROR: unexpected error during writing file: ${e}. Operation aborted.`);
-        return -3;
-    }
-
-    console.log("SUCCESS. Everything is done. Wish you fastest app!");
-    return 0;
 }(_file, _deferify));
