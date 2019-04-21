90f9f747413b89687a4fc20869b29996990f163f
drag13 <qwark@list.ru>
2019-04-21 13:49:11 +0300

    Replacing mechanism changed for working one

--- deferify.js
+++ deferify.js
@@ -1,10 +1,28 @@
 /**
  * @param {string} html 
  */
-function insert(html) {
-    if (!html) { return ''; }
-    const result = html.replace(/(<script\b[^src][^defer][^async][^>]*)>/gm, '$1 defer>');
-    return result;
+function deferify(html) {
+    if (html == null || html === '') { return ''; }
+    return html
+        .split('<script')
+        .map((line, i) => !!i ? insertDeferAttr(line) : line)
+        .join('<script');
 }
 
-module.exports = insert;
+/**
+ * Inserts defer attribute inside script tag if needed
+ * @param {string} line 
+ * @returns Returns new line
+ */
+function insertDeferAttr(line) {
+
+    const isTransformable =
+        line.includes('src') &&
+        line.includes('>') &&
+        !line.includes('defer') &&
+        !line.includes('async');
+
+    return isTransformable ? line.replace(/([^>]*)(>)/, '$1 defer>') : line;
+}
+
+module.exports = deferify;
