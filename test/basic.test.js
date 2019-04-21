af7f557226c783411e113473ea16a45e4286e47d
drag13 <qwark@list.ru>
2019-04-21 13:49:34 +0300

    Added more tests including from the real world

--- test/basic.test.js
+++ test/basic.test.js
@@ -3,8 +3,8 @@ const defer = require('../deferify');
 
 const testSuit = [
     {
-        input: `<script test src="">`,
-        expected: `<script test src="" defer>`
+        input: `<script test="" src="">`,
+        expected: `<script test="" src="" defer>`
     }
     , {
         input: `<script src="">`,
@@ -14,11 +14,11 @@ const testSuit = [
         input: `<script src="zzz"></script><script src="zxc">`,
         expected: `<script src="zzz" defer></script><script src="zxc" defer>`
     }
-    ,{
+    , {
         input: `<script src="zzz"></script><script src="sdfsd">`,
         expected: `<script src="zzz" defer></script><script src="sdfsd" defer>`
     }
-    ,{
+    , {
         input: `<script>test</`,
         expected: `<script>test</`
     },
@@ -26,13 +26,27 @@ const testSuit = [
         input: `<script defer src="zxc"></script>`,
         expected: `<script defer src="zxc"></script>`
     }
-    ,{
+    , {
         input: `<script async src="zxc"></script>`,
         expected: `<script async src="zxc"></script>`
     },
     {
         input: `gghjg <script defer>hhh</script>`,
         expected: `gghjg <script defer>hhh</script>`
+    },
+    {
+        input: ` <script type src=""></script>`,
+        expected: ` <script type src="" defer></script>`
+    },
+    {
+        input: `  <app-root></app-root>
+        <script type="text/javascript" src="runtime.26209474bfa8dc87a77c.js"></script><script type="text/javascript" src="es2015-polyfills.c5dd28b362270c767b34.js" nomodule></script><script type="text/javascript" src="polyfills.8bbb231b43165d65d357.js"></script><script type="text/javascript" src="main.c0a378aac987222c736c.js"></script></body>`,
+        expected: `  <app-root></app-root>
+        <script type="text/javascript" src="runtime.26209474bfa8dc87a77c.js" defer></script><script type="text/javascript" src="es2015-polyfills.c5dd28b362270c767b34.js" nomodule defer></script><script type="text/javascript" src="polyfills.8bbb231b43165d65d357.js" defer></script><script type="text/javascript" src="main.c0a378aac987222c736c.js" defer></script></body>`,
+    },
+    {
+        input: undefined,
+        expected: ''
     }
 ];
 
